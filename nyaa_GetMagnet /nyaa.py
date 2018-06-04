f2158e748108cf3ba93cb43cd1b007a9dd62a17d
Gdist <39858937+Gdist@users.noreply.github.com>
2018-06-04 19:44:02 +0800

    Create nyaa.py

---
+++ nyaa_GetMagnet /nyaa.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+def get_magnet(url):
+	response = requests.get(url)
+	response.encoding = 'UTF-8' 
+	soup = BeautifulSoup(response.text, 'lxml')
+	articles = soup.find_all("tr",)
+	articles = articles[1:]
+
+	for i in articles:
+		td = i.find_all("td")
+		title = td[1].find("a").getText()
+		print(title)
+		x = i.find("td","text-center").find_all("a")
+		magnet = x[0].get("href") if "magnet" in x[0].get("href") else x[1].get("href")
+		#print(magnet)
+		List.append([title,magnet])
+
+keyword = "MXGS-"
+page = 1
+num = 14
+url = "https://sukebei.nyaa.si/?f=0&c=2_2&q="+ keyword + "&p=" + str(page)
+List = []
+
+for i in range(num):
+	page = i+1
+	url = "https://sukebei.nyaa.si/?f=0&c=2_2&q="+ keyword + "&p=" + str(page)
+	get_magnet(url)
+
+filename = "nyaa_" + keyword + ".csv"
+
+with open(filename, "w", encoding = "utf8") as data1:
+	for i in List:
+		for j in i:
+			data1.write("%s,"% (j))
+		data1.write("\n")
