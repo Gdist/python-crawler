4f6f8d80f5346286db48073f7cec40f39bff06b5
Gdist <39858937+Gdist@users.noreply.github.com>
2018-09-16 17:19:10 +0800

    Update offcloud.py

--- OffCloud/offcloud.py
+++ OffCloud/offcloud.py
@@ -35,6 +35,7 @@ def get_magnet(url):
 		x = td[2].find_all("a")
 		magnet = x[0].get("href") if "magnet" in x[0].get("href") else x[1].get("href")
 		#print(magnet)
+		time.sleep(3) #暫停
 		api(magnet)
 		if "success" in apijson.keys():
 			checklist(viewid)
