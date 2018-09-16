9c58ec14d9999cc3c16c632e92bd813ea1969dee
Gdist <39858937+Gdist@users.noreply.github.com>
2018-09-16 17:05:20 +0800

    Update offcloud.py

--- OffCloud/offcloud.py
+++ OffCloud/offcloud.py
@@ -1,4 +1,4 @@
-import requests , json
+import requests , json , time
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 
@@ -19,17 +19,17 @@ def get_magnet(url):
 		seeders = int(td[5].getText())
 		if "#comments" in viewid:
 			continue
-		logNprint("FindID : "+viewid)
-		logNprint("Title  : "+title)
+		logNprint("Find ID : "+viewid)
+		logNprint("Title   : "+title)
 
 		if viewid in ChcekList:
-			logNprint("*Error : "+viewid+" has been added to OffCloud")
+			logNprint("*Error  : "+viewid+" has been added to OffCloud")
 			continue
 		elif seeders < seednum :
-			logNprint("*Error : "+viewid+" has too few seeders (<"+str(seednum)+")")
+			logNprint("*Error  : "+viewid+" has too few seeders (<"+str(seednum)+")")
 			continue
 		'''elif (begin_date - date2 > timedelta(seconds= 0) ) :
-			logNprint("*Error : "+viewid+" Time is too far")
+			logNprint("*Error  : "+viewid+" Time is too far")
 			continue'''
 
 		x = td[2].find_all("a")
@@ -38,7 +38,7 @@ def get_magnet(url):
 		api(magnet)
 		if "success" in apijson.keys():
 			checklist(viewid)
-			logNprint("Success: " +viewid)
+			logNprint("Success : " +viewid)
 
 
 def	api(apilink):
@@ -56,9 +56,9 @@ def checklist(text):
 	with open("checklist.log","a", encoding = 'utf8') as data:
 		data.write(str(text)+"\n")
 
-num = 3 #爬行頁數
+num = 1 #爬行頁數
 seednum = 5 #最低需求上傳種子數
-apikey = "LjK7VjZAJY2ANlND7Tc0Px6woCvsV6pB" #填寫OffCloud API
+apikey = "" #填寫OffCloud API
 '''begin = "2018/09/16 00" #早於此日期的將不會被下載
 begin_date = datetime.strptime(begin, '%Y/%m/%d %H')'''
 
@@ -68,11 +68,14 @@ with open("checklist.log" , "r") as clog: #ID紀錄
 with open("keyword.txt" , "r") as keydata: #ID紀錄
 	KeyList = [l.strip() for l in keydata ]
 
+localtime = time.asctime( time.localtime(time.time()))
+logNprint("\nBeginTime: "+localtime)
 for i in KeyList:
 	keyword = i
 	for j in range(num):
-		print(keyword)
+		logNprint("KeyWord : "+keyword+"\n")
 		page = j+1
 		url = "https://sukebei.nyaa.si/?f=0&c=0_0&q="+ keyword + "&p=" + str(page)
 		#url = "https://sukebei.nyaa.si/?f=0&c=0_0&q="+ keyword + "&s=seeders&o=desc" + "&p=" + str(page)
 		get_magnet(url)
+		break
